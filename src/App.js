ec71389ad6993a7e1c6a9d3e11c6d73f2a81751d
medini123 <115324443+medini123@users.noreply.github.com>
2022-12-28 10:43:31 +0100

    update project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import Profile from "./components/Profile"
+import React from "react";
+import "./App.css";
 
 
-function App() {
-  return (
-    <div className="App"  style={{backgroundColor:"pink"}}>
+class App extends React.Component {
+  state = {
+    show: true,
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show,
+    });
+  };
+
+  render() {
     
-      <Profile />
-      
-    </div>
-  );
+    return (
+      <div>
+      <>
+        <button
+          onClick={this.handleShowPerson}
+          style={{
+            width: "50%",
+            height: "30px",
+            marginTop: "5%",
+            backgroundColor: "pink",
+            border: "none",
+            borderRadius: "100px",
+          }}
+        >
+          click 
+        </button>
+
+        {this.state.show ? <Profile /> : null}
+      </>
+     
+      </div>
+    );
+    
+  
+  }
+  
 }
 
 export default App;
+
